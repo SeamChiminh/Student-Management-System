cc95ce5e7460fb7f24e8e172aecca8a0a0db9b8c
seamchiminh <chiminh096@gmail.com>
2024-05-04 16:53:55 +0700

    create class Student

---
+++ src/fileIO/utils/Student.java
@@ -0,0 +1,11 @@
+package fileIO.utils;
+
+import java.util.Date;
+
+public class Student {
+    private Integer id;
+    private String name;
+    private Date dob;
+    private String classroom;
+    private String subject;
+}
