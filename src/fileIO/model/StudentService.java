11c37aac78106bb56b07a2c12b833035a6e56a52
seamchiminh <chiminh096@gmail.com>
2024-05-05 15:16:52 +0700

    design banner and menu

--- src/fileIO/model/StudentService.java
+++ src/fileIO/model/StudentService.java
@@ -1,17 +1,17 @@
 package fileIO.model;
 
 public interface StudentService {
-    void generateDesignedObjectForWriting();
-    void addNewStudent(Student student);
-    List<Student> ListAllStudent();
-    void commitDataToFile();
-    void commitFromTransaction();
-    void destroyData();
-    Boolean checkIfDataAvailableInTransaction(String fileToCheck);
-    List<Student> serchStudentById(String id);
-    List<Student> searchStudentByName(String name);
-    Student deleteStudentById(String id);
-    Student deleteStudentByName(String name);
+//    void generateDesignedObjectForWriting();
+//    void addNewStudent(Student student);
+//    List<Student> ListAllStudent();
+//    void commitDataToFile();
+//    void commitFromTransaction();
+//    void destroyData();
+//    Boolean checkIfDataAvailableInTransaction(String fileToCheck);
+//    List<Student> serchStudentById(String id);
+//    List<Student> searchStudentByName(String name);
+//    Student deleteStudentById(String id);
+//    Student deleteStudentByName(String name);
 
 
 }
